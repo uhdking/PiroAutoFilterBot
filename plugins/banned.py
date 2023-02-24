b70c223c5947c64c226b71afb4fed707da9e58a2
uhdking <124318949+uhdking@users.noreply.github.com>
2023-02-25 05:15:37 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -31,7 +31,7 @@ async def grp_bd(bot, message):
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
     k = await message.reply(
-        text=f"<b>CHAT NOT ALLOWED 🐞</b>\n\nMy admins has restricted me from working here ! If you want to know more about it contact @uhdimax !!\nReason : <code>{vazha['reason']}</code>.",
+        text=f"<b>CHAT NOT ALLOWED 🐞</b>\n\nMy admins has restricted me from working here ! If you want to know more about it contact @ORGPremium !!\nReason : <code>{vazha['reason']}</code>.",
         reply_markup=reply_markup)
     try:
         await k.pin()
