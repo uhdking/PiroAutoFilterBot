c9aa97f0871c2d8aa9bc6ba0b59195c3e987d032
uhdking <124318949+uhdking@users.noreply.github.com>
2023-03-06 07:38:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1496,10 +1496,21 @@ async def advantage_spell_chok(client, msg): #modified spell check
     ]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spol#{reqstr1}#close_spellcheck')])
     spell_check_del = await msg.reply_photo(
-        photo=(M_IMG),
+        photo=(SPELL_IMG),
         caption=(script.CUDNT_FND.format(mv_rqst)),
         reply_markup=InlineKeyboardMarkup(btn)
     )
+    if NO_RESULTS_MSG:
+            await client.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, mv_rqst)))
+        k = await msg.reply_photo(
+            photo=M_IMG, 
+            caption=script.I_CUDNT.format(mv_rqst),
+            reply_markup=InlineKeyboardMarkup(button)
+        )
+        await asyncio.sleep(600)
+        await k.delete()
+        return
+
     try:
         if settings['auto_delete']:
             await asyncio.sleep(300)
