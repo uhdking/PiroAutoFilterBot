30802a71e671e86c7e463dc8052188cec3475540
uhdking <124318949+uhdking@users.noreply.github.com>
2023-03-09 20:18:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ M_IMG = environ.get("M_IMG", "https://te.legra.ph/file/ce2103c2c9a160ff1d38f.jpg
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5134179712').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001784051585').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '5134179712').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL','-1001770681379')
