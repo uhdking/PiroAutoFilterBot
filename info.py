c47ed3b225301adb2b31a496b000a576345dea4d
uhdking <124318949+uhdking@users.noreply.github.com>
2023-03-11 12:19:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,10 +37,10 @@ auth_channel = environ.get('AUTH_CHANNEL','-1001819426267')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [-1001861187663]
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '')
+support_chat_id = environ.get('SUPPORT_CHAT_ID')
 reqst_channel = environ.get('REQST_CHANNEL_ID')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
-SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
+SUPPORT_CHAT_ID = [-1001655485024]
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
